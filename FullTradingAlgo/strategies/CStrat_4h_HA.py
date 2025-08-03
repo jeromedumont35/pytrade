6aa8bdb9cb387da6d6540e6474aaab360633f506
jerome <jerome.dumont35@gmail.com>
2025-08-03 17:20:34 +0200

    Possibilité de rajouter dans le DF d'un alt des données venant de celui du BTC

--- FullTradingAlgo/strategies/CStrat_4h_HA.py
+++ FullTradingAlgo/strategies/CStrat_4h_HA.py
@@ -1,10 +1,19 @@
 import pandas as pd
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import CRSICalculator
+import CTransformToPanda
+import CIndicatorsBTCAdder
 
 class CStrat_4h_HA:
-    def __init__(self, interface_trade, risk_per_trade_pct: float = 0.1, stop_loss_ratio: float = 0.98):
+    def __init__(self, interface_trade=None, risk_per_trade_pct: float = 0.1, stop_loss_ratio: float = 0.98):
         self.interface_trade = interface_trade
         self.risk_per_trade_pct = risk_per_trade_pct
         self.stop_loss_ratio = stop_loss_ratio
+        self.transformer =CTransformToPanda.CTransformToPanda(raw_dir="../raw", panda_dir="../panda")
 
     def apply(self, df, symbol, row, timestamp, open_positions):
         actions = []
@@ -44,7 +53,7 @@ class CStrat_4h_HA:
                     "position": open_pos
                 })
 
-        if not open_pos and self.interface_trade.get_available_usdc() > 10:
+        if not open_pos and self.interface_trade and self.interface_trade.get_available_usdc() > 10:
             close = row["close"]
             montant_trade = self.interface_trade.get_available_usdc() * self.risk_per_trade_pct
 
@@ -70,3 +79,38 @@ class CStrat_4h_HA:
                 })
 
         return actions
+
+    def apply_indicators(self, df, is_btc_file):
+        df = df.copy()
+
+        if not isinstance(df.index, pd.DatetimeIndex):
+            raise ValueError("Le DataFrame doit avoir un index temporel (datetime).")
+
+        df = CRSICalculator.RSICalculator(df, period=14,
+                                          close_times=[(3, 59), (7, 59), (11, 59), (15, 59), (19, 59), (23, 59)],
+                                          name="rsi_4h_14").get_df()
+
+        close_times_1h = [(h, 59) for h in range(24)]
+        df = CRSICalculator.RSICalculator(df, period=14, close_times=close_times_1h, name="rsi_1h_14").get_df()
+        df = df.drop(columns=[col for col in df.columns if "avg_" in col])
+
+        window = 240  # 4h = 240 minutes
+        open_4h = df['open'].rolling(window=window).apply(lambda x: x.iloc[0], raw=False)
+        high_4h = df['high'].rolling(window=window).max()
+        low_4h = df['low'].rolling(window=window).min()
+        close_4h = df['close'].rolling(window=window).apply(lambda x: x.iloc[-1], raw=False)
+
+        df['close_4h_HA'] = (open_4h + high_4h + low_4h + close_4h) / 4
+
+        if not is_btc_file:
+            adder = CIndicatorsBTCAdder.CIndicatorsBTCAdder(btc_dir="../panda")
+            df = adder.add_columns(df, ["rsi_4h_14", "close"])
+
+        return df
+
+    def run(self):
+        self.transformer.process_all(self.apply_indicators)
+
+if __name__ == "__main__":
+    strat = CStrat_4h_HA()
+    strat.run()
